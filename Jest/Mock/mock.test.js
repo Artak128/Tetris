2b714d40c8d5fd5908637d7136079a31e748a533
Artak Manukyan <artak.manukyan.g@galaxsys.co>
2022-11-17 19:49:57 +0400

    JEST 04

--- Jest/Mock/mock.test.js
+++ Jest/Mock/mock.test.js
@@ -6,11 +6,14 @@ describe("Map function", () => {
 
   beforeEach(() => {
     array = [1, 2, 3, 5];
-    fn = jest.fn((x) => x ** 2);
+    fn = jest.fn((x) => x ** 2); // Mock-eluc heto karoxanum enq hetevel function-in : Jest-i fn-@ decoratori der a katarum u veradarcnuma mer tvac callback-@ havelyal funkcional avelacnelov, vori mijocovel karoxanuma hetevi tvyal callback-in:
   });
 
   test("Should call callback", () => {
     map(array, fn);
     expect(fn).toBeCalled();
+    expect(fn).toBeCalledTimes(4);
   });
 });
+
+//1:15:10
